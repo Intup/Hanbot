cc50f513584399365d10e8c6a062599a5aac82f4
Intup <54212392+Intup@users.noreply.github.com>
2019-09-13 13:40:47 -0300

    Update?

--- IntCaitlyn/AutoUpdate.lua
+++ IntCaitlyn/AutoUpdate.lua
@@ -1,1 +1,4 @@
---Testing
+local url = 'https://raw.githubusercontent.com/Intup/Internal/master/IntCaitlyn/main.lua'
+local dest = hanbot.luapath..'/IntCaitlyn/main.lua'
+local success = network.download_file(url, dest)
+chat.print('Updates were made to the files: main and common');
